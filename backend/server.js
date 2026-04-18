0d4a4736e4080423e0fa7353f626b8f051bc3f3d
yassir-H <yassir.ugr-4999-17@aau.edu.et>
2026-04-18 12:02:21 -0700

    Update destination details in backend

--- backend/server.js
+++ backend/server.js
@@ -104,9 +104,10 @@ app.post('/api/bookings', authenticate, async (req,res) =>{
 // to fill the db we create a seed route
 app.get('/api/destinations/seed', async (req,res)=>{
     const sampleDestinations = [
-        {title: "paris", description: 'visit eiffel tower', location: 'france', price: 1200, image: 'https://unsplash.com'},
-        {title: "bali retreat", description: 'relax on the beaches ', location: 'indonesia', price: 800, image: 'https://unsplash.com'},
-        {title: "swiss alps", description: 'hiking', location: 'switzerland', price: 1200, image: 'https://unsplash.com'}
+        {title: "paris", description: 'Visit The Eiffel Tower in the city of Love.', location: 'Paris, France', price: 1200, image: 'https://images.unsplash.com/photo-1639519306888-419e98f8a5b6?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZWlmZmVsJTIwdG93ZXIlMjBhdCUyMG5pZ2h0fGVufDB8fDB8fHww'},
+        {title: "Bali Retreat", description: 'Relax on the beautiful island Bali  ', location: ' Bali, Indonesia', price: 800, image: 'https://images.unsplash.com/photo-1539367628448-4bc5c9d171c8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YmFsaXxlbnwwfHwwfHx8MA%3D%3D'},
+        {title: "Swiss Alps", description: 'Want to go hiking?', location: 'Swiss Alps, Switzerland', price: 1200, image: 'https://plus.unsplash.com/premium_photo-1690464561785-e7e82e9ced74?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHN3aXNzJTIwYWxwc3xlbnwwfHwwfHx8MA%3D%3D'},
+        {titlle : "Visit Russia", description : "Enjoy the beautiful Rusky Island", location : "Vladivostok , Russia", price : 1350, image :"https://plus.unsplash.com/premium_photo-1681079526808-84047103b7c7?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=https://unsplash.com/photos/an-aerial-view-of-a-bridge-surrounded-by-clouds--ZlDn9n-_Xk%3D"}
     ];
     await Destination.insertMany(sampleDestinations);
     res.send({message: "db seeded"})
